d3ab1aac1d79ef9b7d48eae2d2b4ca239c131e3f
Michael Fulton <fulto081@umn.edu>
2020-01-14 17:58:55 -0600

    Hopefully functional w/ services

---
+++ src/menu.py
@@ -0,0 +1,60 @@
+#! /usr/bin/python
+
+import rospy
+import yaml
+
+from menu_items import Item, ItemService
+from ar_recog.msg import Tags, Tag
+from std_msgs.msg import Header
+
+class Menu(object): 
+
+    def __init__(self):
+        rospy.init_node("minibot_menu")
+        self.rate = rospy.Rate(30)
+        rospy.Subscriber("/minibot/tags", Tags, self.tag_callback)
+
+        self.items = list()
+        with open(rospy.get_param('/minibot_menu/menu_def_file')) as file:
+            output = yaml.load(file, Loader=yaml.SafeLoader)
+            menu_data = output['menu']
+
+            for item in menu_data:
+                item = item['item']
+
+                if item['type'] == 'rosservice':
+                    self.items.append(ItemService(item))
+
+            for item in self.items:
+                print(item)
+        
+        self.input = None
+        self.input_time = None
+        self.unhandled_input = False
+
+    
+    def tag_callback(self, data):
+        self.input = data.tags[0]
+        self.input_time= data.header.stamp.sec
+
+        if not self.unhandled_input:
+            self.unhandled_input = True
+
+    def menu_update(self):
+        if self.unhandled_input:
+            for idx, item in self.items:
+                if idx == self.input.id:
+                    item.execute()
+                    self.unhandled_input = True
+                    break
+
+
+    def menu_graphical_update(self):
+        pass    
+
+if __name__ == '__main__':
+    m = Menu()
+
+    while not rospy.is_shutdown():
+        m.menu_update()
+        m.rate.sleep()
---
+++ src/menu_items.py
@@ -0,0 +1,35 @@
+import rospy
+import pydoc
+
+class Item(object):
+    def __init__(self, item_yaml):
+        self.name = item_yaml['display']
+
+
+class ItemService(Item):
+    def __init__(self,item_yaml):
+        self.name = item_yaml['display']
+        self.service = item_yaml['service']
+        self.service_class = pydoc.locate(item_yaml['service_class'])
+        
+        rospy.wait_for_service(self.service)
+        self.proxy = rospy.ServiceProxy(self.service, self.service_class)
+
+        self.params = item_yaml['params']
+
+    def __str__(self):
+        return "Item: name=%r type=%r service=%r params=%r"%(self.name, "Service", self.service, self.params)
+
+    def execute(self):
+        self.proxy(**self.params)
+
+
+class ItemLaunch(Item):
+    def __init__(self, item_yaml):
+        self.name = item_yaml['display']
+
+class ItemBag(Item):
+    def __init__(self, item_yaml):
+        self.name = item_yaml['displ[ay']
+
+    
