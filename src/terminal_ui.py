f9149fa4caac5e007cf68fde9e9d82c828869685
Elsa Forberger <forbe140@umn.edu>
2021-03-31 10:54:42 -0500

    terminal ui updates

--- src/terminal_ui.py
+++ src/terminal_ui.py
@@ -1,47 +1,34 @@
 #! /usr/bin/python3
 
+# http://adamlamers.com/post/FTPD9KNRA8CT
+
 import os, sys
 import curses
+import yaml
+
+
+
+
 class UI(object):
+
     def __del__(self):
+        print("GOODBYE")
         curses.endwin()
-        os.system('clear')
+        #os.system('clear')
 
     def __init__(self,):
-        # # create named pipe
-        # ros2ui_path = "/tmp/loco_display_ros2ui"
-        # ui2ros_path = "/tmp/loco_display_ui2ros"
-        # files_made = False
-        # try:
-        #     os.mkfifo(ros2ui_path)
-        #     os.mkfifo(ui2ros_path)
-        # except OSError as e:
-        #     if e.errno == 17:
-        #         files_made = True
-        #     else:
-        #         print("ERROR: Failed to create named pipe. (%s)", e)
-        # else:   
-        #     files_made = True
-
-        # if files_made:
-        #     os.chmod(ros2ui_path, 755)
-        #     os.chmod(ui2ros_path, 755)
-        #     self.ros2ui_pipe = open(ros2ui_path, 'r')
-        #     self.ui2ros_pipe = open(ui2ros_path, 'w')
-
         # set up curses
-        menu = {'title' : 'Curses Menu',
+        menu = {'title' : 'LOCO Menu',
         'type' : 'menu',
-        'subtitle' : 'A Curses menu in Python'}
-        option_1 = {'title' : 'Hello World',
-                    'type' : 'command',
-                    'command' : 'echo Hello World!'}
-        menu['options'] = [option_1]
+        'subtitle' : '(Terminal Version)',
+        'items': []}
 
         self.screen = curses.initscr()
-        self.menu_options = menu
-        self.selected_option = 0
-        self._previously_selected_option = None
+        self.menu_items = menu
+        self.selected_item = 0
+        self._previously_selected_item = None
+        self.highlighted_item = 0
+        self._previously_highlighted_item = None
         self.running = True
 
         #init curses and curses input
@@ -51,100 +38,149 @@ class UI(object):
         curses.curs_set(0) #Hide cursor
         self.screen.keypad(1)
 
-        #set up color pair for highlighted option
+        #set up color pair for highlighted item
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
         self.hilite_color = curses.color_pair(1)
-        self.normal_color = curses.A_NORMAL
+        self.normal_color = curses.A_NORMAL 
 
-    def prompt_selection(self, parent=None):
+        #read in and set up menu
+        with open("../menus/first_menu.yaml") as file:
+            output = yaml.load(file, Loader=yaml.SafeLoader)
+            for i in output['menu']:
+                self.menu_items['items'].append(i['item']['display'])
+
+        # create named pipe
+        ros2ui_path = "/tmp/loco_display_ros2ui"
+        ui2ros_path = "/tmp/loco_display_ui2ros"
+        files_made = False
+        try:
+            os.mkfifo(ros2ui_path)
+            os.mkfifo(ui2ros_path)
+        except OSError as e:
+            if e.errno == 17:
+                files_made = True
+            else:
+                print("ERROR: Failed to create named pipe. (%s)", e)
+        else:   
+            files_made = True
+
+        if files_made:
+            os.chmod(ros2ui_path, 755)
+            os.chmod(ui2ros_path, 755)
+            self.ros2ui_pipe = open(ros2ui_path, 'r')
+            self.ui2ros_pipe = open(ui2ros_path, 'w')
+
+    def prompt_selection(self, input_key, parent=None):
+        # print(input_key)
         if parent is None:
-            lastoption = "Exit"
+            lastitem = "Exit"
         else:
-            lastoption = "Return to previous menu ({})".format(parent['title'])
+            lastitem = "Return to previous menu ({})".format(parent['display'])
 
-        option_count = len(self.menu_options['options'])
-
-        input_key = None
+        item_count = len(self.menu_items['items'])
 
         ENTER_KEY = ord('\n')
-        while input_key != ENTER_KEY:
-            if self.selected_option != self._previously_selected_option:
-                self._previously_selected_option = self.selected_option
+        #while input_key != ENTER_KEY:
+        if input_key != ENTER_KEY:
+            self.selected_item = None
 
-            self.screen.border(0)
-            self._draw_title()
-            for option in range(option_count):
-                if self.selected_option == option:
-                    self._draw_option(option, self.hilite_color)
-                else:
-                    self._draw_option(option, self.normal_color)
+            if self.highlighted_item != self._previously_highlighted_item:
+                self._previously_highlighted_item = self.highlighted_item
 
-            if self.selected_option == option_count:
-                self.screen.addstr(5 + option_count, 4, "{:2} - {}".format(option_count+1,
-                    lastoption), self.hilite_color)
-            else:
-                self.screen.addstr(5 + option_count, 4, "{:2} - {}".format(option_count+1,
-                    lastoption), self.normal_color)
-
-            max_y, max_x = self.screen.getmaxyx()
-            if input_key is not None:
-                self.screen.addstr(max_y-3, max_x - 5, "{:3}".format(self.selected_option))
-            self.screen.refresh()
-
-            input_key = self.screen.getch()
+            #input_key = self.screen.getch()
             down_keys = [curses.KEY_DOWN, ord('j')]
             up_keys = [curses.KEY_UP, ord('k')]
             exit_keys = [ord('q')]
 
             if input_key in down_keys:
-                if self.selected_option < option_count:
-                    self.selected_option += 1
+                if self.highlighted_item < item_count:
+                    self.highlighted_item += 1
                 else:
-                    self.selected_option = 0
+                    self.highlighted_item = 0
 
             if input_key in up_keys:
-                if self.selected_option > 0:
-                    self.selected_option -= 1
+                if self.highlighted_item > 0:
+                    self.highlighted_item -= 1
                 else:
-                    self.selected_option = option_count
+                    self.highlighted_item = item_count
 
             if input_key in exit_keys:
-                self.selected_option = option_count #auto select exit and return
-                break
+                self.highlighted_item = item_count #auto select exit and return
+                #break
 
-        return self.selected_option
+        if input_key == ENTER_KEY:
+            self.selected_item = self.highlighted_item
 
-    def _draw_option(self, option_number, style):
-        self.screen.addstr(5 + option_number,
+        #redraw screen
+        self.screen.clear()
+        self.screen.border(0)
+        self._draw_title()
+        for item in range(item_count):
+            if self.highlighted_item == item:
+                # print(item)
+                self._draw_item(item, self.hilite_color)
+            else:
+                # print(item)
+                self._draw_item(item, self.normal_color)
+
+        if self.highlighted_item == item_count:
+            # print(item_count)
+            self.screen.addstr(5 + item_count, 4, "{:2} - {}".format(item_count+1,
+               lastitem), self.hilite_color)
+        else:
+            # print(item_count)
+            self.screen.addstr(5 + item_count, 4, "{:2} - {}".format(item_count+1,
+                lastitem), self.normal_color)
+
+        max_y, max_x = self.screen.getmaxyx()
+        if input_key is not None:
+            # print(input_key)
+            self.screen.addstr(max_y-3, max_x - 5, "{:3}".format(self.highlighted_item))
+        self.screen.refresh()
+
+        return self.selected_item
+
+    def _draw_item(self, item_number, style):
+        self.screen.addstr(5 + item_number,
                            4,
-                           "{:2} - {}".format(option_number+1, self.menu_options['options'][option_number]['title']),
+                           "{:2} - {}".format(item_number+1, self.menu_items['items'][item_number]),#"{:2} - {}".format(item_number+1, self.menu_items['items'][item_number]['display']),
                            style)
 
     def _draw_title(self):
-        self.screen.addstr(2, 2, self.menu_options['title'], curses.A_STANDOUT)
-        self.screen.addstr(4, 2, self.menu_options['subtitle'], curses.A_BOLD)
+        self.screen.addstr(2, 2, self.menu_items['title'], curses.A_STANDOUT)
+        self.screen.addstr(4, 2, self.menu_items['subtitle'], curses.A_BOLD)
 
     def display(self):
-        selected_option = self.prompt_selection()
+        input_key = self.screen.getch()
+        selected_item = self.prompt_selection(input_key)
         i, _ = self.screen.getmaxyx()
 
-        if selected_option < len(self.menu_options['options']):
-            selected_opt = self.menu_options['options'][selected_option]
-            return selected_opt
-        else:
-            self.running = False
-            return {'title' : 'Exit', 'type' : 'exitmenu'}
+        if (selected_item != None):
+            if selected_item < len(self.menu_items['items']):
+                selected_opt = self.menu_items['items'][selected_item]   
+                return selected_opt
+            else:
+                self.running = False
+                return {'title' : 'Exit', 'type' : 'exitmenu'}
+        return None
 
-    def updateMenu(self):
-        menu_length = int(os.read(self.ros2ui_pipe,3)) # read in length of menu update
-        menu_string = os.read(self.ros2ui_pipe,menu_length)
-        print(menu_string)
+    def get_menu_updates(self, menu_string):
+        #menu_length = int(os.read(self.ros2ui_pipe,3)) # read in length of menu update
+        #menu_string = os.read(self.ros2ui_pipe,menu_length)
+        self.menu_items['items'] = menu_string.split()
+
+    def send_menu_selection(self, selected_opt):
+        if os.write(self.ui2ros_pipe, selected_opt) == len(selected_opt):
+            # wrote everything
+            print(":D")
+        else:
+            # uh oh
+            print("oh no, didn't write everything")
 
 
 if __name__=='__main__':
     u = UI()
-    u.display()
+    while (u.running):
+        u.display()
+        u.get_menu_updates("test1 test2 ")
     del u
-    while (1):
-        
-        #u.updateMenu()
